29d68532f715ff0295bfcb93813074e80991df56
Ryan Nguyen <rnguyen513@gmail.com>
2025-03-29 14:30:33 -0400

    add calendar library

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -1,12 +1,25 @@
-import { Chat } from "@/components/custom/chat";
-import { generateUUID } from "@/lib/utils";
+"use client";
+
+// import { Chat } from "@/components/custom/chat";
+// import { generateUUID } from "@/lib/utils";
+import SchedulerWrapper from "@/components/schedule/_components/view/schedular-view-filteration";
+import { SchedulerProvider } from "@/providers/schedular-provider";
 
 export default async function Page() {
 //   const id = generateUUID();
 //   return <Chat key={id} id={id} initialMessages={[]} />;
     return(
-        <div className="flex flex-col">
-            hello test
-        </div>
+        <div className="p-5">
+            <SchedulerProvider weekStartsOn="monday">
+            <SchedulerWrapper 
+            stopDayEventSummary={true}
+            classNames={{
+                tabs: {
+                panel: "p-0",
+                },
+            }}
+            />
+        </SchedulerProvider>
+      </div>
     )
 }
